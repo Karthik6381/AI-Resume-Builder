0f1eb6d231a0943ca079a51883a743e1b63679ba
Karthik6381 <karthike6381@gmail.com>
2026-03-31 20:11:16 +0530

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Banner from '../components/home/Banner'
+import Hero from '../components/home/Hero'
+import Features from '../components/home/Features'
+import CallToAction from '../components/home/CallToAction'
+import Footer from '../components/home/Footer'
+import Title from '../components/home/Title'
+
+
+
+const Home = () => {
+  return (
+    <div>
+        <Banner />
+        <Hero />
+        <Features/>
+        <CallToAction />
+        <Footer />
+        <Title />
+        
+
+        
+    </div>
+  )
+}
+
+export default Home
